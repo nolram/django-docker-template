e84dd113c2298bdb47753f632af75908d1ab9b09
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-01-09 13:27:55 -0200

    - Template files

---
+++ mywebsite/settings/dev.py
@@ -0,0 +1,21 @@
+import os
+
+from mywebsite.settings.common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
