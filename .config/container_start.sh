6195d81fe12f9ec070a2bb978485de88058aca7a
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-01-10 17:31:51 -0200

    - Modificações para o ambiente do Openshift

--- .config/container_start.sh
+++ .config/container_start.sh
@@ -3,4 +3,4 @@ cd /var/projects/mywebsite && python manage.py migrate --no-input
 #cd /var/projects/mywebsite && python manage.py collectstatic --no-input
 #echo `whoiam`
 #echo `namei -mo /var/log/supervisor/supervisord.log`
-#supervisord -n -u mywebsiteuser -c /etc/supervisor/supervisord.conf
+supervisord -n -u mywebsiteuser -c /etc/supervisor/supervisord.conf
