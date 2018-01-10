d8fefec953dc189824f075e248cb11d5a0c61efd
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-01-10 10:52:25 -0200

    - Modificações no Dockerfile e no conf do supervisor

--- .config/container_start.sh
+++ .config/container_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /var/projects/mywebsite && python manage.py migrate --no-input
 #cd /var/projects/mywebsite && python manage.py collectstatic --no-input
-sudo supervisord -n -c /etc/supervisor/supervisord.conf
+supervisord -n -c /etc/supervisor/supervisord.conf
