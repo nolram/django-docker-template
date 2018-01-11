f9d275bc8683b43c2ee35b1ae81c13b8e7354d55
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-01-11 13:49:43 -0200

    - Modificações para o ambiente do Openshift

--- mywebsite/settings/prod.py
+++ mywebsite/settings/prod.py
@@ -20,10 +20,10 @@ DATABASES = {
     }
 }
 
+MIDDLEWARE = MIDDLEWARE + ['whitenoise.middleware.WhiteNoiseMiddleware', ]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
